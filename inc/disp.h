8bfe246f26f8ce4befa7abd14b8068e498786297
99003743 <78868680+99003743@users.noreply.github.com>
2021-02-25 10:12:12 +0530

    Create disp.h

---
+++ inc/disp.h
@@ -0,0 +1,21 @@
+# ifndef _DISPLAY_H_
+# define _DISPLAY_H_
+
+# include"add_last.h"
+
+/**
+ * @brief calculates volume of box
+ * 
+ * @param box1 passes boxes
+ * @return unsigned long volume of a single box
+ */
+void Display(Box *box1);
+
+/**
+ * @brief Displaying the status
+ * 
+ * @param box1 passes all the boxes
+ * @return unsigned long min_volume of all the boxes
+ */
+
+# endif
